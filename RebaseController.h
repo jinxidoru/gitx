9af9b5fe936a2d4912603040129f636a7a6e9836
Viper Bailey <viper@adobe.com>
2012-02-02 15:52:00 -0700

    (rebase) initial work for rebase UI

---
+++ RebaseController.h
@@ -0,0 +1,13 @@
+//
+//  RebaseController.h
+//  GitX
+//
+//  Created by Michael Bailey on 2/2/12.
+//  Copyright 2012 Adobe. All rights reserved.
+//
+
+
+
+@interface RebaseController : NSObject
+
+@end
---
+++ RebaseController.m
@@ -0,0 +1,23 @@
+//
+//  RebaseController.m
+//  GitX
+//
+//  Created by Michael Bailey on 2/2/12.
+//  Copyright 2012 Adobe. All rights reserved.
+//
+
+#import "RebaseController.h"
+
+@implementation RebaseController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
